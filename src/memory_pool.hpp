4c13f5bc5b1de271fa028ed0f000ce3870da9c4a
Aidonmaster <canalbpontop@gmail.com>
2025-04-08 19:57:58 -0300

    Update 0.01

--- src/memory_pool.hpp
+++ src/memory_pool.hpp
@@ -1,50 +1,5 @@
-// #pragma
-
-// #include <cstring>
-
-// #include <memory>   // necessário para std::unique_ptr
-// #include <iostream>
-
-
-// // Tamanho do pool (2MB)
-// constexpr size_t MEMORY_POOL_SIZE = 2 * 1024 * 1024;
-
-// // Tipo de dado que vamos manipular
-// using Data = uint32_t;
-
-
-// class MemoryPool {
-//     public:
-//         MemoryPool() {
-//             memory = std::make_unique<uint8_t[]>(MEMORY_POOL_SIZE);
-//             std::memset(memory.get(), 0, MEMORY_POOL_SIZE);
-//         }
-    
-//         // Escreve um valor na pool
-//         void write(size_t address, Data value) {
-//             if (address + sizeof(Data) > MEMORY_POOL_SIZE) {
-//                 return;
-//             }
-//             std::memcpy(memory.get() + address, &value, sizeof(Data));
-//         }
-    
-//         // Lê um valor da pool
-//         Data read(size_t address) const {
-//             if (address + sizeof(Data) > MEMORY_POOL_SIZE) {
-//                 return 0;
-//             }
-//             Data value;
-//             std::memcpy(&value, memory.get() + address, sizeof(Data));
-//             return value;
-//         }
-    
-//     private:
-//         std::unique_ptr<uint8_t[]> memory;
-// };
-
-
 #pragma once
-// #include <cstdint>
+
 #include <memory>
 #include <cstring>
 #include <iostream>
--- src/register_bank.hpp
+++ src/register_bank.hpp
@@ -1,47 +1,6 @@
-// #pragma
-
-// #include <iostream>
-// #include <array>
-
-// // Número de "registradores virtuais"
-// constexpr size_t NUM_REGISTERS = 40;
-
-// // // Tipo de dado que vamos manipular
-// using Data = uint32_t;
-
-// class RegisterBank {
-//     public:
-//         RegisterBank() {
-//             registers.fill(0);
-//         }
-    
-//         void set(size_t index, Data value) {
-//             if (index < NUM_REGISTERS) registers[index] = value;
-//         }
-    
-//         Data get(size_t index) const {
-//             if (index < NUM_REGISTERS) return registers[index];
-//             return 0;
-//         }
-    
-//         void add(size_t index, Data value) {
-//             if (index < NUM_REGISTERS) registers[index] += value;
-//         }
-    
-//         void debug() const {
-//             for (size_t i = 0; i < NUM_REGISTERS; ++i) {
-//                 std::cout << "R[" << i << "] = " << registers[i] << "\n";
-//             }
-//         }
-    
-//     private:
-//         std::array<Data, NUM_REGISTERS> registers;
-// };
-
-
 #pragma once
+
 #include <array>
-// #include <cstdint>
 #include <iostream>
 
 constexpr size_t NUM_REGISTERS = 40;
